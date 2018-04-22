521d8dbcf7c2b0569c1a73b81691d0580a5de65f
gareth-cheeseman <garethcheeseman@humbug.local>
2018-04-22 16:43:01 +0100

    Makes board generation dynamic

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,18 +1,20 @@
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for (let rowNumber = 0; rowNumber < numberOfRows; rowNumber++) {
+    let row = [];
+    for (let columnNumber = 0; columnNumber < numberOfColumns; columnNumber++) {
+      row.push(" ");
+    }
+    board.push(row);
+  }
+  return board;
+};
+
 const printBoard = board => {
-    console.log('Current board:');
-    board.forEach(row => {
-        console.log(row.join(' | '))
-    });
-}
+  console.log("Current board:");
+  board.forEach(row => {
+    console.log(row.join(" | "));
+  });
+};
 
-const board = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' ']
-];
-
-printBoard(board);
-
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+printBoard(generatePlayerBoard(3, 3));
