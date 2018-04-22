570c99ccb46d41170c6b6f087f5d350f323d8a98
gareth-cheeseman <garethcheeseman@humbug.local>
2018-04-22 17:16:17 +0100

    Makes bomb board dynamic and updates printBoard

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -3,18 +3,39 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   for (let rowNumber = 0; rowNumber < numberOfRows; rowNumber++) {
     let row = [];
     for (let columnNumber = 0; columnNumber < numberOfColumns; columnNumber++) {
-      row.push(" ");
+      row.push(' ');
     }
     board.push(row);
   }
   return board;
 };
 
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (let rowNumber = 0; rowNumber < numberOfRows; rowNumber++) {
+    let row = [];
+    for (let columnNumber = 0; columnNumber < numberOfColumns; columnNumber++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+  let numberOfBombsPlaced = 0;
+  while(numberOfBombsPlaced < numberOfBombs){
+      let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+      let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+      board[randomRowIndex][randomColumnIndex] = 'B';
+      numberOfBombsPlaced++;
+      //can add bombs on top of bombs.
+  }
+
+
+
+  return board;
+};
+
 const printBoard = board => {
-  console.log("Current board:");
-  board.forEach(row => {
-    console.log(row.join(" | "));
-  });
+  console.log('Current board:');
+  console.log(board.map(row => row.join(' | ')).join('\n'));
 };
 
 printBoard(generatePlayerBoard(3, 3));
