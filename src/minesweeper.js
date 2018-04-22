7c22cbeb3b4958da13e985bc5f06ffc9e52abac6
gareth-cheeseman <garethcheeseman@humbug.local>
2018-04-22 08:07:17 +0100

    Adds hard-coded game board.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -3,11 +3,16 @@ const printBoard = board => {
     board.forEach(row => {
         console.log(row.join(' | '))
     });
-
 }
+
 const board = [
     [' ', ' ', ' '],
     [' ', ' ', ' '],
     [' ', ' ', ' ']
 ];
+
+printBoard(board);
+
+board[0][1] = '1';
+board[2][2] = 'B';
 printBoard(board);
